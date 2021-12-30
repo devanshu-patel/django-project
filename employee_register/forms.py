3c7a7c46e33993bbd04481809ecb0f1cc6ce1784
devanshu652 <96855956+devanshu652@users.noreply.github.com>
2021-12-30 11:05:55 +0530

    Add files via upload

---
+++ employee_register/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Employee
+
+class EmployeeForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ('fullname','mobile','emp_code','position')
+    def __init__(self,*args,**kwargs):
+        super(EmployeeForm,self).__init__(*args,**kwargs)
+        self.fields['position'].empty_label="Select Position"
+   
